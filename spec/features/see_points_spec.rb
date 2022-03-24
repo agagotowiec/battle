fd0b611615fda2896ce48ff98624d67a5e1f4d25
agagotowiec <agnieszka.gtwc@gmail.com>
2022-03-24 18:06:08 +0000

    implemented switching turns

--- spec/features/see_points_spec.rb
+++ spec/features/see_points_spec.rb
@@ -1,6 +1,10 @@
 feature 'See points' do
-  scenario 'view Player2 hit points' do
+  scenario 'see Player2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
+  end
+  scenario 'see Player1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Charlotte: 60HP'
   end
 end
